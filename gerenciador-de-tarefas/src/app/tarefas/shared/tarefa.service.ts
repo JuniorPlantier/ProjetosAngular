9e669005fc3c033f4dfb5bd800b84128c76c4303
Junior Plantier <junior.plantier@gmail.com>
2019-12-09 10:12:29 -0200

    Implementado serviços das tarefas

--- gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
+++ gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -22,6 +22,40 @@ export class TarefaService {
     localStorage['tarefas'] = JSON.stringify(tarefas); /* Retorna uma string */
   }
 
-  
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    // find: fica dentro de uma lista; ele itera em cada Tarefa e verifica a condição. 
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  atualizar(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    // itera sobre as tarefas, verificar se o id é o mesmo e atualiza;
+    // forEach(Objeto, Posição da Tarefa, Lista de Objetos)
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    // filter: itera sobre as tarefas, e retorna os objetos de acordo com o filtro aplicado.
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].concluida = !obj.concluida;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
 
 }
