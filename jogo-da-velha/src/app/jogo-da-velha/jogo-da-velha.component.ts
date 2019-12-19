ee09edf3872da78d985a6ab3d1e249300ba04be1
Junior Plantier <junior.plantier@gmail.com>
2019-12-19 11:09:34 -0200

    Add funcionalidades nas views

--- jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { JogoDaVelhaService } from './shared';
+
 @Component({
   selector: 'app-jogo-da-velha',
   templateUrl: './jogo-da-velha.component.html',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
 
   ngOnInit() {
+    this.jogoDaVelhaService.inicializar();
   }
+
+  // Retorna se a tela de início deve ser exibida.
+  get showInicio(): boolean {
+    return this.jogoDaVelhaService.ShowInicio;
+  }
+
+  // Retorna se o tabuleiro deve ser exibida.
+  get showTabuleiro(): boolean {
+    return this.jogoDaVelhaService.ShowTabuleiro;
+  }
+
+  // Retorna se a tela de fom de jogo deve ser exibida.
+  get showFinal(): boolean {
+    return this.jogoDaVelhaService.ShowFinal;
+  }
+
+  // botão "Iniciar Jogo"
+  iniciarJogo(): void {
+    this.jogoDaVelhaService.iniciarJogo();
+  }
+
+  // realiza uma jogada, ao clicar em algum local no tabuleiro.
+  jogar(posX: number, posY: number): void{
+    this.jogoDaVelhaService.jogar(posX, posY);
+  }
+
+  // para exibir a peça X
+  exibirX(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  // para exibir a peça O
+  exibirO(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+ // retorna se a marcação da vitória deve ser exibida
+ exibirVitoria(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirVitoria(posX, posY);
+ }
 
 }
